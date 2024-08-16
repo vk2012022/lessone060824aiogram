3a2d51a2ab85d71aaef986237f5d0cdc7a76239b
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-08-16 13:24:36 +0300

    add new code 2

--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram. filters import CommandStart, Command
-from aiogram. types import Message, FSInputFile
-from aiogram. fsm. context import FSMContext
-from aiogram. fsm.state import State, StatesGroup
-from aiogram. fsm. storage. memory import MemoryStorage
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+import requests
 
+# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
 from config import API_TOKEN
-import sqlite3
-import aiohttp
-import logging
+
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 async def main():
-await dp.start_polling(bot)
+    await dp.start_polling(bot)
 
-if __name__ == '__main__':
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
